e7470f2863331c749ef4bd70dcb6e6e4eacd29a1
Priya-Manchanda <98579874+Priya-Manchanda@users.noreply.github.com>
2023-08-08 20:03:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,11 +7,7 @@ const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public/public"));
 app.get("/favicon.ico", (req, res) => {
